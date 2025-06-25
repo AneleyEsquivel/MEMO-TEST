2f6c1634ff67e04861e5b3107a0b476f899178a1
AneleyEsquivel <aneleyesquivel@gmail.com>
2025-06-25 16:56:37 -0300

    js

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
+const elementos = ["🍷", "🍹", "🍺", "🍻", "🥂", "🍾"];
+let cartas = [];
+let primeraCarta = null;
+let segundaCarta = null;
+let bloqueado = false;
+let aciertos = 0;
 
+const tablero = document.getElementById("cartas");
+const mensaje = document.getElementById("FIN");
+const botonReiniciar = document. getElementById("reiniciar-btn");
+
+const mezclar = array => array.sort(()=> Math.random() - 0.5);
+
+const crearTablero = () => {
+    tablero.innerHTML = "";
+    mensaje.textContent = "";
+    aciertos = 0;
+    primeraCarta = null;
+    segundaCarta = null;
+    bloqueado = false;
+
+    cartas = [...elementos, ...elementos];
+    cartas = mezclar(cartas);
+
+    cartas.forEach((icono) => {
+        const carta = document.createElement("div");
+        carta.classList. add ("carta");
+        carta.dataset.icono = icono;
+        carta.textContent = "";
+
+        carta.addEventListener("click",() =>{
+            if (bloqueado) return;
+            if (carta === primeraCarta) return;
+            if (carta.textContent !== "") return;
+            carta.textContent = icono;
+
+            if(!primeraCarta) {
+                primeraCarta = carta;
+            }else{
+                segundaCarta = carta;
+                bloqueado = true;
+            }
+        })
+    })
+}
